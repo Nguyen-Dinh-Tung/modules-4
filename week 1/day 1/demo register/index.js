c55952df80a9229222b596c22153f4592f85129f
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-09-07 10:09:00 +0700

    update

---
+++ week 1/day 1/demo register/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const multer = require("multer");
+const upload = multer();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("register");
+});
+const arrUser = [];
+app.post("/user/register", upload.none(), (req, res) => {
+  if (req.body.username && req.body.password) {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+    };
+    arrUser.push(user);
+    console.log(arrUser);
+    res.render("success", {user: user});
+  } else {
+    res.render("error");
+  }
+});
+app.listen(port, () => {
+  console.log("listening " + port);
+});
