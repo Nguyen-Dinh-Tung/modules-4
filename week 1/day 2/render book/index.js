bbded28488d16b6f9ca73412047e4779e79bbea2
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-09-08 09:00:15 +0700

    update

---
+++ week 1/day 2/render book/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+const multer = require("multer");
+const upload = multer();
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123123",
+  database: "ebook",
+});
+app.set("view engine", "ejs");
+app.set("views", "./views");
+const selectSql = `select * from book`;
+
+app.get("/", (req, res) => {
+  connection.query(selectSql, (err, data) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+    res.render("read", {data: data});
+  });
+});
+app.listen(port, () => {
+  console.log("listening " + port);
+});
