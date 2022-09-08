e48b293072272eab347b7be248df9ea4d8a7c7a4
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-09-08 15:05:41 +0700

    update

---
+++ week 1/day 3/demo limit web/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+const multer = require("multer");
+const upload = multer();
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123123",
+  database: "company",
+});
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  const selectSql = `select * from staff limit 10`;
+  connection.query(selectSql, (err, data) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+    console.log(data);
+    res.render("home", {data: data});
+  });
+});
+app.get("/create", (req, res) => {
+  res.render("create");
+});
+app.post("/create", upload.none(), (req, res) => {
+  let name = req.body.name;
+  let age = req.body.age;
+  let position = req.body.position;
+  console.log(position);
+  const insertSql = `insert into staff(name , age , position) values ('${name}' , ${age} , '${position}')`;
+  connection.query(insertSql, (err) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+    console.log("insert success");
+    res.redirect("/");
+  });
+});
+app.get("/details", (req, res) => {
+  let id = Number(req.query.id);
+  const selectSql = `select * from staff where id = ${id}`;
+  connection.query(selectSql, (err, data) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+    res.render("details", {data: data});
+  });
+});
+app.get("/delete", (req, res) => {
+  let id = Number(req.query.id);
+  const selectSql = `delete from staff where id = ${id}`;
+  connection.query(selectSql, (err, data) => {
+    if (err) {
+      throw new Error(err.message);
+    }
+    res.redirect("/");
+  });
+});
+app.listen(port, () => {
+  console.log("listening " + port);
+});
