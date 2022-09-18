799e4abb3e967cce0eba340c1b2d7d55c60543be
Nguyen-Dinh-Tung <s30.tung@gmail.com>
2022-09-18 14:19:34 +0700

    update

--- week 2/day 3/demo products list/dist/src/router/products.router.js
+++ week 2/day 3/demo products list/dist/src/router/products.router.js
@@ -9,10 +9,17 @@ const multer_1 = __importDefault(require("multer"));
 const bookRoutes = (0, express_1.Router)();
 const upload = (0, multer_1.default)();
 const productsRouter = (0, express_1.Router)();
+let limit = 3;
+let offset = 0;
 productsRouter.get('/products/list', async (req, res) => {
-    let data = await products_model_1.Products.find();
-    console.log(data);
-    res.render('listProducts', { data: data });
+    let data = await products_model_1.Products.find().limit(limit).skip(offset);
+    res.render('listProducts', { data: data, limit: limit, offset: offset });
+});
+productsRouter.get('/list', async (req, res) => {
+    limit = Number(req.query.limit);
+    offset = Number(req.query.offset);
+    let data = await products_model_1.Products.find().limit(limit).skip(offset);
+    res.render('listProducts', { data: data, limit: limit, offset: offset });
 });
 productsRouter.get('/products/create', (req, res) => {
     res.render('create');
--- week 2/day 3/demo products list/src/router/products.router.ts
+++ week 2/day 3/demo products list/src/router/products.router.ts
@@ -6,13 +6,18 @@ const bookRoutes = Router() ;
 const upload = multer() ;
 
 const productsRouter = Router() ;
-
+let limit = 3 ;
+let offset = 0 ;
 productsRouter.get('/products/list' ,async (req,res) =>{
-  let data = await Products.find()
-  console.log(data);
-  res.render('listProducts' , {data : data})
+  let data = await Products.find().limit(limit).skip(offset)
+  res.render('listProducts' , {data : data , limit:limit , offset : offset})
 })
-
+productsRouter.get('/list' , async(req,res) =>{
+  limit = Number(req.query.limit) ;
+  offset = Number(req.query.offset)
+  let data = await Products.find().limit(limit).skip(offset)
+  res.render('listProducts' , {data : data , limit:limit , offset : offset})
+})
 productsRouter.get('/products/create' , (req,res) =>{
   res.render('create')
 })
